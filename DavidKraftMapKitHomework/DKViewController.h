a0d4e413bc27fb85795067deaf7697df7fed4624
David Kraft <kraftd@goldmail.etsu.edu>
2014-04-14 13:57:43 -0400

    Initial Commit

---
+++ DavidKraftMapKitHomework/DKViewController.h
@@ -0,0 +1,13 @@
+//
+//  DKViewController.h
+//  DavidKraftMapKitHomework
+//
+//  Created by David Kraft on 4/14/14.
+//  Copyright (c) 2014 David Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DKViewController : UIViewController
+
+@end
---
+++ DavidKraftMapKitHomework/DKViewController.m
@@ -0,0 +1,29 @@
+//
+//  DKViewController.m
+//  DavidKraftMapKitHomework
+//
+//  Created by David Kraft on 4/14/14.
+//  Copyright (c) 2014 David Kraft. All rights reserved.
+//
+
+#import "DKViewController.h"
+
+@interface DKViewController ()
+
+@end
+
+@implementation DKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
